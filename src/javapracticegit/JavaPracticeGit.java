828d574557e1bdbb8deb526fe48cb93563840c6a
@Saloni <>
2023-02-02 20:47:16 +0530

    SecondGitCommit

--- src/javapracticegit/JavaPracticeGit.java
+++ src/javapracticegit/JavaPracticeGit.java
@@ -15,7 +15,7 @@ public class JavaPracticeGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Welcome Home");
+        System.out.println("Welcome Home Bye");
     }
     
 }
